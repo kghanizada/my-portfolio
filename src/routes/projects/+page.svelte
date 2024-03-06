68aa1853486d6bf7a833a51d680455fa07d7bda4
kghanizada <kg7952@mit.edu>
2024-03-05 19:11:34 -0500

    changes

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,34 +1,24 @@
+<svelte:head>
+	<title>My Projects</title>
+</svelte:head>
+
 <script>
-    // Import any necessary modules or define variables here
-  </script>
-  
-  <main>
-    <h1>Projects</h1>
+    import projects from '$lib/projects.json';
+</script>
+
+<main>
+    <h1>My Projects ({ projects.length })</h1>
     <div class="projects">
-      <article>
-        <a href="../Visualizations/A2_report.html">
-          <h2>Assignment 2: Exploratory Data Analysis</h2>
-          <img class="fit-picture" src="../Visualizations/Fig 8.png" alt="">
-          <p>
-            <!-- Replace lorem! with the actual content -->
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </a>
-      </article>
-      <article>
-        <a href="../A3/A3_report.html">
-          <h2>Assignment 3: Visualization Design</h2>
-          <img class="fit-picture" src="../A3/Fig 4.jpg" alt="">
-          <p>
-            <!-- Replace lorem! with the actual content -->
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </a>
-      </article>
-      <!-- Add the rest of your articles here -->
-    </div>
-  </main>
-  
-  <style>
+            {#each projects as p}
+                <article>
+                    <a href={p.url}>
+                    <h2>{p.title}</h2>
+                    <img class="fit-picture" src={p.image} alt="">
+                    <p>{p.description}</p>
+                </article>
+            {/each}
+        </div>
+</main>
+<style>
     /* Add any necessary styles here */
-  </style>
+</style>
--- static/global.js
+++ static/global.js
@@ -26,7 +26,7 @@ for (let p of pages) {
 	let title = p.title;
 	// TODO create link and add it to nav
 	
-    url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+    //url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
 
 	let a = document.createElement("a");
 	a.href = url;
